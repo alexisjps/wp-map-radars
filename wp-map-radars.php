2992ccaee5989a732c92dbf6f6ed7795c32b2130
Alexis <alexis@astephan.digital>
2024-12-03 20:24:43 +0100

    add html filters
    Signed-off-by: Alexis <alexis@astephan.digital>

--- wp-map-radars.php
+++ wp-map-radars.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: WP Map Radars
- * Description: Plugin pour afficher des radars sur une carte Mapbox avec des markers spécifiques.
- * Version: 1.0
+ * Description: Plugin pour afficher des radars sur une carte Mapbox avec des markers spécifiques et des options de filtre.
+ * Version: 1.1
  * Author: Alexis Stephan
  */
 
@@ -14,7 +14,7 @@ if (!defined('ABSPATH')) {
 function wp_map_radars_enqueue_scripts() {
     wp_enqueue_script('mapbox-gl', 'https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.js', [], '2.15.0', true);
     wp_enqueue_style('mapbox-gl-css', 'https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css', [], '2.15.0');
-    wp_enqueue_script('wp-map-radars', plugins_url('js/map.js', __FILE__), ['mapbox-gl', 'jquery'], '1.0', true);
+    wp_enqueue_script('wp-map-radars', plugins_url('js/map.js', __FILE__), ['mapbox-gl', 'jquery'], '1.1', true);
     wp_enqueue_style('wp-map-radars-css', plugins_url('css/style.css', __FILE__));
 
     // Passer l'URL du fichier CSV et la clé API Mapbox au script JS
@@ -27,8 +27,14 @@ function wp_map_radars_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'wp_map_radars_enqueue_scripts');
 
-// Shortcode pour afficher la map
+// Shortcode pour afficher la map et les filtres
 function wp_map_radars_shortcode() {
-    return '<div id="map" style="width: 100%; height: 500px;"></div>';
+    return '
+        <div id="radar-filters" style="margin-bottom: 20px;">
+            <label><input type="checkbox" value="Radar fixe" checked> Radar fixe</label>
+            <label><input type="checkbox" value="Radar feu rouge" checked> Radar feu rouge</label>
+        </div>
+        <div id="map" style="width: 100%; height: 500px;"></div>
+    ';
 }
 add_shortcode('wp_map_radars', 'wp_map_radars_shortcode');
